f3356a6ea810539354d04367264669fe817081fd
GCGHVCJY <166798493+GCGHVCJY@users.noreply.github.com>
2024-09-03 14:29:07 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+input.onButtonPressed(Button.A, function () {
+    bitbot.ledRainbow(true, BBArms.Both)
+    bitbot.gocm(BBDirection.Forward, 100, 50)
+    bitbot.rotatems(BBRobotDirection.Left, 20, 1000)
+})
